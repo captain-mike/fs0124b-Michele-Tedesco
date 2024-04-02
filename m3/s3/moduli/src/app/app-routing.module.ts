6f8811b90016fc412415e877bdc056f51a54e18a
captain-mike <info@grafikweb.it>
2024-04-02 13:02:25 +0200

    guard

--- m3/s3/moduli/src/app/app-routing.module.ts
+++ m3/s3/moduli/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './pages/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
-  { path: 'chi-siamo', loadChildren: () => import('./pages/chi-siamo/chi-siamo.module').then(m => m.ChiSiamoModule) },
+  {
+    path: 'chi-siamo',
+    loadChildren: () => import('./pages/chi-siamo/chi-siamo.module').then(m => m.ChiSiamoModule),
+    canActivate : [AuthGuard],
+    canActivateChild : [AuthGuard]
+  },
   {
     path: 'contatti',
     loadChildren: () => import('./pages/contatti/contatti.module').then(m => m.ContattiModule)
