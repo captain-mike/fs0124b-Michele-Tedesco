cb93e14da001aa0aaa2fcd1cb8e0640c5d839c61
captain-mike <info@grafikweb.it>
2024-04-04 12:32:16 +0200

    auth

--- m3/s3/g3/auth/src/app/auth/auth.service.ts
+++ m3/s3/g3/auth/src/app/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { BehaviorSubject, map } from 'rxjs';
-import { IUser } from '../Modules/i-user';
+import { BehaviorSubject, Observable, map, tap } from 'rxjs';
+import { IUser } from '../Models/i-user';
+import { HttpClient } from '@angular/common/http';
+import { ILoginData } from '../Models/i-login-data';
+import { environment } from '../../environments/environment.development';
+import { Router } from '@angular/router';
 
 type AccessData = {
   accessToken:string,
@@ -22,5 +26,69 @@ export class AuthService {
   //!!user è come scrivere Boolean(user)
   //isLoggedIn$ = this.user$.pipe(map(user => Boolean(user)))
 
-  constructor() { }
+  constructor(
+    private http:HttpClient,
+    private router:Router
+    ) {
+
+      this.restoreUser()
+
+    }
+
+  //ng g environments
+  registerUrl:string = environment.registerUrl
+  loginUrl:string = environment.loginUrl
+
+  register(newUser:Partial<IUser>):Observable<AccessData>{
+    return this.http.post<AccessData>(this.registerUrl,newUser)
+  }
+
+  login(loginData:ILoginData):Observable<AccessData>{
+    return this.http.post<AccessData>(this.loginUrl,loginData)
+    .pipe(tap(data => {
+
+      this.authSubject.next(data.user)//comunico al subject che l'utente si è loggato
+      localStorage.setItem('accessData', JSON.stringify(data))
+
+      this.autoLogout(data.accessToken)
+
+    }))
+  }
+
+
+  logout(){
+
+    this.authSubject.next(null)//comunico al subject che l'utente si è sloggato
+    localStorage.removeItem('accessData')//cancello i dati dell'utente
+
+    this.router.navigate(['/auth/login'])//mando via l'utente loggato
+
+  }
+
+  autoLogout(jwt:string){
+    const expDate = this.jwtHelper.getTokenExpirationDate(jwt) as Date;//trovo la data di scadenza del token
+    const expMs = expDate.getTime() - new Date().getTime(); //sottraggo i ms della data/ora di oggi da quella nel jwt
+
+    //avvio un timer, quando sarà passato il numero di ms necessari per la scadenza del token, avverrà il logout
+    setTimeout(()=>{
+      this.logout()
+    },expMs)
+  }
+
+
+  restoreUser(){
+
+    const userJson = localStorage.getItem('accessData')//recupero io dati di accesso
+    if(!userJson) return; //se l'utente non si è mai loggato blocca tutto
+
+    const accessData:AccessData = JSON.parse(userJson)//se viene eseguita questa riga significa che i dati ci sono, quindi la converto da json ad oggetto per permetterne la manipolazione
+    if(this.jwtHelper.isTokenExpired(accessData.accessToken)) return; //se il token è scaduto blocco tutto
+
+
+    this.authSubject.next(accessData.user)
+    this.autoLogout(accessData.accessToken)
+
+  }
+
+
 }
